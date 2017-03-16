ae7411c2b104c1bb374f7c3bf64b6b83d542d0b2
emeric31 <emeric_31@hotmail.fr>
2017-03-16 15:01:13 +0000

    update

--- js/javascript.js
+++ js/javascript.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
     $(window).scroll(fixDiv);
     fixDiv();
 });
+
 //-------------------------------SMOOTH-SCROLL----------------------------------------------
 
 $(function() {
